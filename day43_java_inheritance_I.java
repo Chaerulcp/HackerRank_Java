c96eef761d14320776066c1b0d00f3bf46aaa01e
Chaerulcp <indonesianvidios@gmail.com>
2022-04-02 17:03:04 +0800

    Java Inheritance I

---
+++ day43_java_inheritance_I.java
@@ -0,0 +1,37 @@
+// java 15
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Animal{
+    void walk()
+    {
+        System.out.println("I am walking");
+    }
+}
+class Bird extends Animal implements asd{
+    void fly(){
+        System.out.println("I am flying");
+    }
+    public void sing() {
+        System.out.println("I am singing");
+    }
+}
+interface asd{
+    void sing();
+}
+
+public class day43_java_inheritance_I {
+
+   public static void main(String args[]){
+
+      Bird bird = new Bird();
+      bird.walk();
+      bird.fly();
+      bird.sing();
+    
+   }
+}
