bc8f50b6b58d03f3aceaa85de089b30dd32f08dc
Chaerulcp <indonesianvidios@gmail.com>
2022-04-14 00:23:15 +0800

    ava Reflection Attributes

---
+++ day54_java_reflection_attributes.java
@@ -0,0 +1,19 @@
+// java 7
+
+public class day54_java_reflection_attributes {
+
+    public static void main(String[] args){
+        Class student = Student.class;
+        Method[] methods = student.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for(Method method : methods){
+            methodList.add(method.getName());
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+
+}
