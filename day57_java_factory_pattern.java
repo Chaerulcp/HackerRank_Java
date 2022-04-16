0e0d709b9b2556f5c76921a301b2f410dd601dd3
Chaerulcp <indonesianvidios@gmail.com>
2022-04-16 12:12:01 +0800

    Java Factory Pattern

---
+++ day57_java_factory_pattern.java
@@ -0,0 +1,81 @@
+// java 15
+
+import java.io.*;
+import java.util.*;
+import java.security.*;
+
+interface Food {
+     public String getType();
+    }
+    class Pizza implements Food {
+     public String getType() {
+     return "Someone ordered a Fast Food!";
+     }
+    }
+
+    class Cake implements Food {
+
+     public String getType() {
+     return "Someone ordered a Dessert!";
+     }
+    }
+    class FoodFactory {
+        public Food getFood(String order) {
+
+switch (order){
+     case "pizza": return new Pizza();
+     case "cake" : return new Cake();
+     default : return null;
+}
+
+}//Akhir dari getFood method
+
+    }//Akhir dari factory class
+
+    public class day57_java_factory_pattern {
+
+     public static void main(String args[]){
+            Do_Not_Terminate.forbidExit();
+
+        try{
+
+            Scanner sc=new Scanner(System.in);
+            
+            FoodFactory foodFactory = new FoodFactory();
+    
+            Food food = foodFactory.getFood(sc.nextLine());
+    
+            
+            System.out.println("The factory returned "+food.getClass());
+            System.out.println(food.getType());
+        }
+        catch (Do_Not_Terminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+     }
+
+    }
+    class Do_Not_Terminate {
+         
+        public static class ExitTrappedException extends SecurityException {
+
+            private static final long serialVersionUID = 1L;
+        }
+     
+        public static void forbidExit() {
+            final SecurityManager securityManager = new SecurityManager() {
+                @Override
+                public void checkPermission(Permission permission) {
+                    if (permission.getName().contains("exitVM")) {
+                        throw new ExitTrappedException();
+                    }
+                }
+            };
+            System.setSecurityManager(securityManager);
+        }
+    }    
+        
+
+
+
+
